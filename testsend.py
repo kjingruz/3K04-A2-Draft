684f559b374ebaff77abaf9f22cb1af29020e79d
kjingruz <jingruzhang0802@gmail.com>
2022-11-14 16:05:35 -0500

    change the ser.write input

--- testsend.py
+++ testsend.py
@@ -1,7 +1,7 @@
 import serial
 
-ser = serial.Serial('/dev/tty.usbmodem0000001234561')
+ser = serial.Serial('/dev/tty.usbmodem0000001234561',9600)
 print(ser.name)
-ser.write(b'hello')
+ser.write(1)
 ser.close()
 print("test file")
