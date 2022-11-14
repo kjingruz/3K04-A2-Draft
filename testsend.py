4c07350d3d6efbcd5608f15d4248b08f7d100648
kjingruz <jingruzhang0802@gmail.com>
2022-11-14 17:08:53 -0500

    DONE textsend, it worked

--- testsend.py
+++ testsend.py
@@ -1,7 +1,12 @@
 import serial
 
-ser = serial.Serial('/dev/tty.usbmodem0000001234561',9600)
+ser = serial.Serial('/dev/tty.usbmodem0000001234561',115200)
 print(ser.name)
-ser.write(1.0)
+enter = [0x16, 0x55, 0x1, 0x1, 0x0, 0x3] #dec
+# hexnum = []
+# for i in enter:
+#     hexnum.append(hex(i))
+#ser.open()
+ser.write(enter)
 ser.close()
 print("test file")
