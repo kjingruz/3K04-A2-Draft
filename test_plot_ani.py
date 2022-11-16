9d90f4bd71c8db6fb4a941070e8f063e078264f4
Stefan P <praniaus@mcmaster.ca>
2022-11-16 17:10:45 -0500

    updating_plot_and_live_write

--- test_plot_ani.py
+++ test_plot_ani.py
@@ -1,25 +1,34 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import time
+import random
+#import time
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
-inc = 1
 
 def addToFile():
-     f = open('sampleText.txt', 'a')
-     f.write('\n13,2\n14,7')
+
+     f = open('sampleText.txt', 'r')
+     xval,yval = f.readline().split(',')
      f.close()
-     inc+=1
-     print(inc)
+
+     xnum = int(xval)
+     ynum = int(yval)
+
+
+     f = open('sampleText.txt', 'a')
+    # writeVal = str(xnum)+","+str(ynum)+'\n'
+     writeVal = str(random.randrange(0,10))+','+str(random.randrange(0,10))+'\n'
+     f.write(writeVal)
+     f.close()
+     
 
 def animate(i):
 
-    #addToFile()#update vals
+    addToFile()#update vals
 
-    pullData = open("sampleText.txt","r")
-    pullData.read()                                 # must use txt file for input, else graph wont update
+    pullData = open("sampleText.txt","r").read()                                 # must use txt file for input, else graph wont update
     #pullData.close()
 
     dataArray = pullData.split('\n')
@@ -32,5 +41,8 @@ def animate(i):
             yar.append(int(y))
     ax1.clear()
     ax1.plot(xar,yar)
+
+
+
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
