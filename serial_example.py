341bd1b717153250e948ac3e3b9659e57fb40760
Stefan P <praniaus@mcmaster.ca>
2022-11-26 20:53:09 -0500

    Update serial_example.py

--- serial_example.py
+++ serial_example.py
@@ -5,16 +5,30 @@ import struct
 # Mac ports, for windows you have to find the ports yourself
 frdm_port = "/dev/cu.usbmodem0000001234561"
 
+# B = uint8
+# f = single
+# H = uint16
+
 Start = b'\x16'
 SYNC = b'\x22'
 Fn_set = b'\x55'
-red_en = struct.pack("B", 1)
-green_en = struct.pack("B", 0)
-blue_en = struct.pack("B", 1)
-off_time = struct.pack("B", 1)
+Pacing_mode = struct.pack("B", 1)
+LRL = struct.pack("B", 1) 
+URL = struct.pack("B", 1)
+MSR = struct.pack("B", 1)
+A_V_PA = struct.pack("f", 1.0)
+A_V_PW = struct.pack("B", 1) 
+A_V_Sense = struct.pack("B", 1)
+A_V_R = struct.pack("H", 1)
+PVARP = struct.pack("H", 1)
+Act_thres = struct.pack("B", 1) 
+React_time = struct.pack("B", 1)
+Response_factor = struct.pack("B", 1)
+Recovery_time = struct.pack("B", 1)
 
-Signal_set = Start + Fn_set + red_en + green_en + blue_en + off_time
-Signal_echo = Start + SYNC + red_en + green_en + blue_en + off_time
+
+Signal_set = Start + Fn_set + Pacing_mode + LRL + URL + MSR + A_V_PA + A_V_PW + A_V_Sense + A_V_R + PVARP + Act_thres + React_time + Response_factor + Recovery_time
+Signal_echo = Start + SYNC + Pacing_mode + LRL + URL + MSR + A_V_PA + A_V_PW + A_V_Sense + A_V_R + PVARP + Act_thres + React_time + Response_factor + Recovery_time
 
 with serial.Serial(frdm_port, 115200) as pacemaker:
     pacemaker.write(Signal_set)
